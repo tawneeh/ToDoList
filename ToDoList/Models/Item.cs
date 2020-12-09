e6a5b70babe826f4ca0cc8a1a08a5f0d04ff4086
Tawnee Harris <tawneeh@icloud.com>
2020-12-08 21:57:10 -0800

    add static method

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,18 +5,19 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; } 
-    private static List<Item> _instances = new List<Item> {}; // static variable. maintains a list of all Item objects 
+    private static List<Item> _instances = new List<Item> {}; // static variable. maintains a list of all Item objects
 
     public Item(string description) // constructor - pretty sure
     {
       Description = description;
       _instances.Add(this);
+      Item.GetAll();
     }
 
     public static List<Item> GetAll()
     {
       return _instances;
-      // Item.GetAll() this would call the GetAll() method directly on the class
     }
+
   }
 }
