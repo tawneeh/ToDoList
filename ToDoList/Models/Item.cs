aaa6b1de48ec885c8c97f9b65af89b3d83e4ff81
Tawnee Harris <tawneeh@icloud.com>
2020-12-08 21:31:16 -0800

    add a static variable

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic; // needed to access Lists
 namespace ToDoList.Models
+
 {
   public class Item
   {
-    public string Description { get; }
+    public string Description { get; set; } 
+    private static List<Item> _instances = new List<Item> {}; // static variable. maintains a list of all Item objects 
 
     public Item(string description) // constructor - pretty sure
     {
       Description = description;
+      _instances.Add(this);
+    }
 
+    public static List<Item> GetAll()
+    {
+      return _instances;
+      // Item.GetAll() this would call the GetAll() method directly on the class
     }
   }
 }
