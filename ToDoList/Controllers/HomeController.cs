4254739f21c043caf14416164dc366538e1a6a64
Tawnee Harris <tawneeh@icloud.com>
2020-12-14 22:53:05 -0800

    lessons 23 thru 26

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,29 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
 
-    [Route("/")] // "/" means 'root route' or Homepage
+    [HttpGet("/")]
     public ActionResult Index()
-    {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
-    }
-
-    [Route("/items/new")] // this renders the form (CreateForm.cshtml)
-    public ActionResult CreateForm()
     {
       return View();
     }
 
-    [Route("/items")] // this tells the controller what to do when the form is submitted (make a new To Do List Item)
-    public ActionResult Create(string description) // 'description' because it matches the name attribute of the form
-    {
-      Item myItem = new Item(description);
-      return View("Index", myItem); // this renders a new Item at the "Index" view. Basically rerouting back to the Homepage
-    }
   }
 }
