dafa23904bc6e64464cda254747b52e149e84bc3
Tawnee Harris <tawneeh@icloud.com>
2020-12-20 13:39:07 -0800

    update code to test the test database

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using ToDoList.Models;
 using System; 
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Tests
 {
@@ -13,6 +14,12 @@ namespace ToDoList.Tests
     {
       Item.ClearAll();
     }
+
+    public ItemTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -21,37 +21,46 @@ namespace ToDoList.Models
 
     public static List<Item> GetAll()
     {
-        List<Item> allItems = new List<Item> { };
-        MySqlConnection conn = DB.Connection();
-        conn.Open(); // each time we make a query, we need to open a new database connection. lines 19 and 20
-        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-        cmd.CommandText = @"SELECT * FROM items;"; // lines 21 and 22: this is the actual SQL query. selecting all entries from the items table. The query is stored in a special object called a MySqlCommand. MySqlConnection needs a MySqlCommand
-        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader; // this Data Reader Object is responsible for reading the data returned by our database in response to the query command
-        while (rdr.Read()) // build-in Read() method (built-in to the MySqlDataReader object). Reads results one at a time from the database and returns a boolean; when it reaches the end of the database records, it returns false and the while loop ends
-        { // this code block below turns each row in our database into an Item stored in a List that our application understands. lesson 6
-            int itemId = rdr.GetInt32(0);
-            string itemDescription = rdr.GetString(1);
-            Item newItem = new Item(itemDescription, itemId); // must add an overloaded constructor to Item so that Items retrieved from the database can contain their Id
-            allItems.Add(newItem);
-        }
-        conn.Close();
-        if (conn != null)
-        {
-            conn.Dispose();
-        }
-        return allItems; // returns the list of all items
+      List<Item> allItems = new List<Item> { };
+      MySqlConnection conn = DB.Connection();
+      conn.Open(); // each time we make a query, we need to open a new database connection. lines 19 and 20
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;"; // lines 21 and 22: this is the actual SQL query. selecting all entries from the items table. The query is stored in a special object called a MySqlCommand. MySqlConnection needs a MySqlCommand
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader; // this Data Reader Object is responsible for reading the data returned by our database in response to the query command
+      while (rdr.Read()) // build-in Read() method (built-in to the MySqlDataReader object). Reads results one at a time from the database and returns a boolean; when it reaches the end of the database records, it returns false and the while loop ends
+      { // this code block below turns each row in our database into an Item stored in a List that our application understands. lesson 6
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId); // must add an overloaded constructor to Item so that Items retrieved from the database can contain their Id
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems; // returns the list of all items
     }
 
     public static void ClearAll()
     {
-      // empty for now
+      MySqlConnection conn = DB.Connection(); // start with a new database connection
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items;";
+      cmd.ExecuteNonQuery(); // denotes a SQL statement that modifies data instead of querying and returning
+      conn.Close(); // and close the database connection
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
 
     public static Item Find(int searchId)
     {
-    // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with a database.
-    Item placeholderItem = new Item("placeholder item");
-    return placeholderItem;
+      // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with a database.
+      Item placeholderItem = new Item("placeholder item");
+      return placeholderItem;
     }
 
   }
