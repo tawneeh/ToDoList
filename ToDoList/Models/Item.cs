25f315915679aeb0ad4a72c17ba6256a392f4a69
Tawnee Harris <tawneeh@icloud.com>
2020-12-20 13:16:30 -0800

    add code to retrieve Items from the database

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,33 +1,57 @@
 using System.Collections.Generic; // needed to access Lists
-namespace ToDoList.Models
+using MySql.Data.MySqlClient;
 
+namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set; } 
     public int Id { get; }
-    private static List<Item> _instances = new List<Item> {}; // static variable. maintains a list of all Item objects
 
     public Item(string description) // constructor
     {
       Description = description;
-      _instances.Add(this);
-      Id = _instances.Count; // this needs to be after _instances.Add(this) otherwise it won't have _instances to .Count
+    }
+
+    public Item(string description, int id) // overloaded constructor 
+    {
+      Description = description;
+      Id = id;
     }
 
     public static List<Item> GetAll()
     {
-      return _instances;
+        List<Item> allItems = new List<Item> { };
+        MySqlConnection conn = DB.Connection();
+        conn.Open(); // each time we make a query, we need to open a new database connection. lines 19 and 20
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT * FROM items;"; // lines 21 and 22: this is the actual SQL query. selecting all entries from the items table. The query is stored in a special object called a MySqlCommand. MySqlConnection needs a MySqlCommand
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader; // this Data Reader Object is responsible for reading the data returned by our database in response to the query command
+        while (rdr.Read()) // build-in Read() method (built-in to the MySqlDataReader object). Reads results one at a time from the database and returns a boolean; when it reaches the end of the database records, it returns false and the while loop ends
+        { // this code block below turns each row in our database into an Item stored in a List that our application understands. lesson 6
+            int itemId = rdr.GetInt32(0);
+            string itemDescription = rdr.GetString(1);
+            Item newItem = new Item(itemDescription, itemId); // must add an overloaded constructor to Item so that Items retrieved from the database can contain their Id
+            allItems.Add(newItem);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+        return allItems; // returns the list of all items
     }
 
     public static void ClearAll()
     {
-      _instances.Clear();
+      // empty for now
     }
 
     public static Item Find(int searchId)
     {
-      return _instances[searchId-1]; // -1 because Id starts at 1 but _instances index starts at 0
+    // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with a database.
+    Item placeholderItem = new Item("placeholder item");
+    return placeholderItem;
     }
 
   }
