03a4cdd4172ca1863eac1254703c066802e28fbe
Tawnee Harris <tawneeh@icloud.com>
2020-12-21 22:54:12 -0800

    update ItemsController

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -18,14 +18,25 @@ namespace ToDoList.Controllers
     {
       List<Item> model = _db.Items.ToList(); // this replaces GetAll()
       return View(model);
+
+// db is an instance of our DbContext class. It's holding a reference to our database
+// Once there, it looks for an object named Items. This is the DbSet we declared in ToDoListContext.cs
+// LINQ turns this DbSet into a list using the ToList() method, which comes from the System.Linq namespace
+// This expression is what creates the model we'll use for the Index view
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
-
-// db is an instance of our DbContext class. It's holding a reference to our database.
-
-// Once there, it looks for an object named Items. This is the DbSet we declared in ToDoListContext.cs.
-
-// LINQ turns this DbSet into a list using the ToList() method, which comes from the System.Linq namespace.
-
-// This expression is what creates the model we'll use for the Index view.
