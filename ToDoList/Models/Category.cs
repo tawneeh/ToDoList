7bbb308fbfe2877714d0492057e8c81403292eaf
Tawnee Harris <tawneeh@icloud.com>
2020-12-23 11:08:53 -0800

    add code from lessons 29 and 30

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -4,38 +4,15 @@ namespace ToDoList.Models
 {
   public class Category
   {
-    private static List<Category> _instances = new List<Category> {};
+
+    public Category()
+    {
+        this.Items = new HashSet<Item>(); // HashSet is an unordered collection of unique elements. Like a List but can't have duplicates
+    }
+
+    public int CategoryId { get; set; }
     public string Name { get; set; }
-    public int Id { get; }
-    public List<Item> Items { get; set; }
-
-    public Category(string categoryName) // constructor
-    {
-      Name = categoryName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Items = new List<Item>{}; // object within object
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
-    public static List<Category> GetAll()
-    {
-      return _instances;
-    }
-
-    public static Category Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
-
-    public void AddItem(Item item)
-    {
-      Items.Add(item);
-    }
+    public virtual ICollection<Item> Items { get; set; } // ICollection is required by Entity. It outlines methods for querying and changing data which Entity needs to work its magic.
 
   }
 }
