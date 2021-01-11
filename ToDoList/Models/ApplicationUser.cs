3b23c831416f97e0951b5c0e55a73338eb07765f
Tawnee Harris <tawneeh@icloud.com>
2021-01-10 17:37:45 -0800

    add comments

--- ToDoList/Models/ApplicationUser.cs
+++ ToDoList/Models/ApplicationUser.cs
@@ -2,8 +2,11 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ToDoList.Models
 {
-  public class ApplicationUser : IdentityUser
+  public class ApplicationUser : IdentityUser // extending ApplicationUser with IdentityUser allows access to necessary Identity functuinality and properties - Email, UserName, failed login attempts, etc
   {
 
   }
 }
+
+// ApplicationUser is a class that represents user accounts
+// Identity is built to work with Entity and therefore databases
