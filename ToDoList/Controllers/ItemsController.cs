704b22fd67e0c7ab19842c6d4579f5c791ad480e
Tawnee Harris <tawneeh@icloud.com>
2021-01-11 08:48:02 -0800

    add comments

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,17 +3,17 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq; // LINQ is short for Language-Integrated Query
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Rendering; // gives access to SelectList
 
 //new using directives
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
-using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization; 
+using Microsoft.AspNetCore.Identity; // to interact with users
+using System.Threading.Tasks; // to call async methods
+using System.Security.Claims; // claim based authorization
 
 namespace ToDoList.Controllers
 {
-  [Authorize] //new line
+  [Authorize] // allows access to ItemsController only if a user is logged in!
   public class ItemsController : Controller
   {
     private readonly ToDoListContext _db;
@@ -28,15 +28,15 @@ namespace ToDoList.Controllers
 
     public async Task<ActionResult> Index()
     {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // 'this' refers to the ItemController itself. ClaimTypes.NameIdentifier located the unique ID associated with the current account. the ? in this line is an existential operator. It states that we should only call the method to the right of the ? if the method to the left of the ? doesn't return null
       var currentUser = await _userManager.FindByIdAsync(userId);
-      var userItems = _db.Items.Where(entry => entry.User.Id == currentUser.Id);
+      var userItems = _db.Items.Where(entry => entry.User.Id == currentUser.Id); // we use the Where() method, which is a LINQ method we can use to query a collection in a way that echoes the logic of SQL. We can use Where() to make many different kinds of queries, as the method accepts an expression to filter our results. simply asking Entity to find items in the database where the user id associated with the item is the same id as the id that belongs to the currentUser. This ensures users only see their own tasks in the view
       return View(userItems);
     }
 
     public ActionResult Create()
     {
-      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name"); // this enables an Item to belong to a category that already exists
       return View();
     }
 
@@ -44,7 +44,7 @@ namespace ToDoList.Controllers
     public async Task<ActionResult> Create(Item item, int CategoryId)
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
+      var currentUser = await _userManager.FindByIdAsync(userId); // find the current user, then add to the db
       item.User = currentUser;
       _db.Items.Add(item);
       if (CategoryId != 0)
@@ -57,9 +57,9 @@ namespace ToDoList.Controllers
 
     public ActionResult Details(int id)
     {
-      var thisItem = _db.Items
-          .Include(item => item.Categories)
-          .ThenInclude(join => join.Category)
+      var thisItem = _db.Items // List of Item objects from the database
+          .Include(item => item.Categories) // .Include loads the Categories property of each Item
+          .ThenInclude(join => join.Category) // .ThenInclude loads the Category of each CategoryItem. Kind of confusing - lesson 5
           .FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
@@ -107,7 +107,7 @@ namespace ToDoList.Controllers
       return View(thisItem);
     }
 
-    [HttpPost, ActionName("Delete")]
+    [HttpPost, ActionName("Delete")] // ActionName("Delete:") refers to the above Delete method that this Post is using. This action actually deletes the item with .Remove()
     public ActionResult DeleteConfirmed(int id)
     {
       var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
